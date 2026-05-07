7d606ecb4c81f4a95e8765691901ac821eebe656
DanailIvanov21 <101013894+DanailIvanov21@users.noreply.github.com>
2026-05-07 15:11:05 +0300

    Create Building.h

---
+++ Week11/Solutions/task03/Building.h
@@ -0,0 +1,32 @@
+#pragma once
+#include <iostream>
+#include "MyString.h"
+
+class Building {
+private:
+    int height;       
+    double area;       
+    MyString address;  
+
+public:
+    Building();
+    Building(int h, double a, const MyString& addr);
+
+    
+    Building(const Building& other) = default;
+    Building& operator=(const Building& other) = default;
+    ~Building() = default;
+
+    
+    int getHeight() const;
+    double getArea() const;
+    const MyString& getAddress() const;
+
+   
+    void setHeight(int h);
+    void setArea(double a);
+    void setAddress(const MyString& addr);
+
+    friend std::ostream& operator<<(std::ostream& os, const Building& b);
+    friend std::istream& operator>>(std::istream& is, Building& b);
+};
