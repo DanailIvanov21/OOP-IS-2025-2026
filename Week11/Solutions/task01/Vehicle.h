81ab16acd3f546250db936e98ba0610bca67305b
DanailIvanov21 <101013894+DanailIvanov21@users.noreply.github.com>
2026-05-07 14:17:25 +0300

    Create Vehicle.h

---
+++ Week11/Solutions/task01/Vehicle.h
@@ -0,0 +1,35 @@
+#pragma once
+#include <iostream>
+#include "MyString.h"
+#include "Color.h"
+
+class Vehicle {
+private:
+    Color color;
+    MyString brand;
+    int year;
+    int seats;
+    int maxSpeed;
+
+public:
+    Vehicle();
+    Vehicle(Color c, const MyString& b, int y, int s, int m);
+
+    //Vehicle(const Vehicle& other) = default;
+    //Vehicle& operator=(const Vehicle& other) = default;
+    //~Vehicle() = default;
+
+    Color getColor() const;
+    const MyString& getBrand() const;
+    int getYear() const;
+    int getSeats() const;
+    int getMaxSpeed() const;
+
+    void setColor(Color c);
+    void setBrand(const MyString& b);
+    void setYear(int y);
+    void setSeats(int s);
+    void setMaxSpeed(int m);
+
+    friend std::ostream& operator<<(std::ostream& os, const Vehicle& v);
+};
