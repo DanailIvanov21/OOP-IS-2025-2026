03d7c2269ac16299b03749e274a33e0e93f3381b
DanailIvanov21 <101013894+DanailIvanov21@users.noreply.github.com>
2026-05-13 14:54:32 +0300

    Update main.cpp

--- Week12/Solutions/task03/main.cpp
+++ Week12/Solutions/task03/main.cpp
@@ -2,23 +2,27 @@
 #include "PC.h"
 #include "Laptop.h"
 #include "GamingConsole.h"
+#include "ComputerCollection.h"
 
 int main() {
+    ComputerCollection shop;
+
     PC pc(3.5, "RTX 3060", 650, 16);
     Laptop lap(2.8, "Intel Iris", 120, 8);
     GamingConsole gc(3.2, "Custom GPU", 200, 16);
 
-    Computer* arr[3] = { &pc, &lap, &gc };
+    shop.add(pc);
+    shop.add(lap);
+    shop.add(gc);
 
-    for (int i = 0; i < 3; i++) {
-        arr[i]->printType();
-        size_t count;
-        const MyString* per = arr[i]->getPeripherals(count);
-        std::cout << "Peripherals: ";
-        for (size_t j = 0; j < count; j++)
-            std::cout << per[j] << " ";
-        std::cout << "\n\n";
-    }
+    std::cout << "All computers:\n";
+    shop.printAll();
+
+    std::cout << "Removing by GPU model RTX 3060...\n";
+    shop.removeByVideo("RTX 3060");
+
+    std::cout << "After removal:\n";
+    shop.printAll();
 
     return 0;
 }
