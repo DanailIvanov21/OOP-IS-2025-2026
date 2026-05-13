3e4d40615b749b1961bc175fd7af742bcb6d2942
DanailIvanov21 <101013894+DanailIvanov21@users.noreply.github.com>
2026-05-13 11:59:48 +0300

    Create Device.h

---
+++ Week12/Solutions/task01/Device.h
@@ -0,0 +1,32 @@
+#pragma once
+#include <iostream>
+
+class Device {
+protected:
+    char* model;
+    char* brand;
+    int price;
+
+    void copyFrom(const Device& other);
+    void free();
+
+public:
+    Device();
+    Device(const char* model, const char* brand, int price);
+    Device(const Device& other);
+    Device& operator=(const Device& other);
+    virtual ~Device();
+
+    const char* getModel() const;
+    const char* getBrand() const;
+    int getPrice() const;
+
+    void setModel(const char* m);
+    void setBrand(const char* b);
+    void setPrice(int p);
+
+    virtual void print(std::ostream& os) const = 0;
+    virtual void saveToFile(std::ostream& os) const = 0;
+    virtual void loadFromFile(std::istream& is) = 0;
+    virtual Device* clone() const = 0;
+};
